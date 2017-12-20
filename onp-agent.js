e425e1f81c927267309357f8187f4e8e7dc5004d
open-novel <sp.hikaru@docomo.ne.jp>
2017-12-20 17:33:34 +0900

    us

---
+++ onp-agent.js
@@ -0,0 +1,46 @@
+/*
+These codes are licensed under CC0.
+http://creativecommons.org/publicdomain/zero/1.0
+*/
+
+{
+
+window.addEventListener( 'DOMContentLoaded', ( ) => setTimeout( init, 1 ) )
+
+
+function init ( ) {
+
+	let elms = Array.from( document.getElementsByClassName( 'onp' ) )
+
+	elms.forEach( e => { e.addEventListener( 'click', onp ) } )
+
+}
+
+
+
+async function onp ( e ) {
+
+	e.preventDefault( )
+	e.stopPropagation( )
+
+	let url = e.target.getAttribute( 'href' )
+	if ( ! url ) return
+
+	let address = e.target.dataset.onpURL || 'https://open-novel.github.io'
+
+	let player = window.open( address )
+
+	player.onload = async ( ) => {
+
+		let buf = await ( await fetch( url ) ).arrayBuffer( )
+
+		player.postMessage( { type: 'install-packed', version: '1.0', file: buf }, '*', [ buf ] )
+
+	}
+
+}
+
+
+window.onp = onp
+
+}
